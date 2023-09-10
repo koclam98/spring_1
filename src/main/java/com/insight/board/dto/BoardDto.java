bba9d1883be5c78bb07be9f360014b72c2d562cf
chaRan <bkffjqb10@gmail.com>
2023-09-10 13:17:30 +0900

    20230910_dto추가

---
+++ src/main/java/com/insight/board/dto/BoardDto.java
@@ -0,0 +1,17 @@
+package com.insight.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDto {
+
+    private int boardIdx;
+    private String title;
+    private String contents;
+    private int hitCnt;
+    private String creatorId;
+    private String createdDatetime;
+    private String updateId;
+    private String updateDatetime;
+
+}
